8a9012c6518acfb1fcd2696cc0b658e39567a2b7
HardWar3 <mikewegner@gmx.net>
2022-06-07 20:48:04 +0200

    user struct mit variablen nach PAP und einige eigenen fürs programm

---
+++ new_user_daten.cpp
@@ -0,0 +1,183 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "user_daten.hpp"
+
+struct user_daten* new_user_daten() {
+
+	struct user_daten* user;
+
+	user = ( user_daten* ) calloc( 1, sizeof( *user ));
+
+	assert( user );
+
+//---------------------------------------------------------------------------------------------------------------------
+//	Eingabeparameter
+//---------------------------------------------------------------------------------------------------------------------
+
+	user->id_nummer			= 1;
+	user->af				= 0;
+	user->ajahr				= 2000;
+	user->alter1			= 0;
+	user->entsch			= 0;
+	user->f					= 0;
+	user->jfreib			= 0;
+	user->jhinzu			= 0;
+	user->jre4				= 0;
+	user->jre4ent			= 0;
+	user->jvbez				= 0;
+	user->krv				= 0;
+	user->kvz				= 0;
+	user->lzz				= 0;
+	user->lzzfreib			= 0;
+	user->lzzhinzu			= 0;
+	user->mbv				= 0;
+	user->pkpv				= 0;
+	user->pkv				= 0;
+	user->pvs				= 0;
+	user->pvz				= 0;
+	user->r					= 0;
+	user->re4				= 0;
+	user->sonstb			= 0;
+	user->sonstent			= 0;
+	user->sterbe			= 0;
+	user->stkl				= 1;
+	user->vbez				= 0;
+	user->vbezm				= 0;
+	user->vbezs				= 0;
+	user->vbs				= 0;
+	user->vjahr				= 0;
+	user->vkapa				= 0;
+	user->vmt				= 0;
+	user->zkf				= 0;
+	user->zmvb				= 0;
+
+//---------------------------------------------------------------------------------------------------------------------
+// 	Ausgabeparameter
+//---------------------------------------------------------------------------------------------------------------------
+	
+	user->bk				= 0;
+	user->bks				= 0;
+	user->bkv				= 0;
+	user->lstlzz			= 0;
+	user->solzlzz			= 0;
+	user->solzs				= 0;
+	user->solzv				= 0;
+	user->sts				= 0;
+	user->stv				= 0;
+	user->vkvlzz			= 0;
+	user->vkvsonst			= 0;
+
+//---------------------------------------------------------------------------------------------------------------------
+// interne Felder or Ausgbeparameter DBA
+//---------------------------------------------------------------------------------------------------------------------
+
+	user->vfrb				= 0;
+	user->vfrbs1			= 0;
+	user->vfrbs2			= 0;
+	user->wvfrb				= 0;
+	user->wvfrbm			= 0;
+	user->wvfrbo			= 0;
+
+//---------------------------------------------------------------------------------------------------------------------
+// 	Interne Felder
+//---------------------------------------------------------------------------------------------------------------------
+
+	user->alte				= 0;
+	user->anp				= 0;
+	user->anteil1			= 0;
+	user->bbgkvpv			= 0;
+	user->bbgrv				= 0;
+	user->bmg				= 0;
+	user->diff				= 0;
+	user->efa				= 0;
+	user->fvb				= 0;
+	user->fvbso				= 0;
+	user->fvbz				= 0;
+	user->fvbzso			= 0;
+	user->gfb				= 0;
+	user->hbalte			= 0;
+	user->hfvb				= 0;
+	user->hfvbz				= 0;
+	user->hfvbzso			= 0;
+	user->hoch				= 0;
+	user->j					= 0;
+	user->jbmg				= 0;
+	user->jlfreib			= 0;
+	user->jlhinzu			= 0;
+	user->jw				= 0;
+	user->k					= 0;
+	user->kennvmt			= 0;
+	user->kfb				= 0;
+	user->kvsatzag			= 0;
+	user->kvsatzan			= 0;
+	user->kztab				= 0;
+	user->lstjahr			= 0;
+	user->lst1				= 0;
+	user->lst2				= 0;
+	user->lst3				= 0;
+	user->lstoso			= 0;
+	user->lstso				= 0;
+	user->mist				= 0;
+	user->pvsatzag			= 0;
+	user->pvsatzan			= 0;
+	user->rvsatzan			= 0;
+	user->rw				= 0;
+	user->sap				= 0;
+	user->solzfrei			= 0;
+	user->solzj				= 0;
+	user->solzmin			= 0;
+	user->solzsbmg			= 0;
+	user->solzszve			= 0;
+	user->solzvbmg			= 0;
+	user->st				= 0;
+	user->st1				= 0;
+	user->st2				= 0;
+	user->stovmt			= 0;
+	user->tab1				= 0;
+	user->tab2				= 0;
+	user->tab3				= 0;
+	user->tab4				= 0;
+	user->tab5				= 0;
+	user->tbsvorv			= 0;
+	user->vbezb				= 0;
+	user->vbezbso			= 0;
+	user->vergl				= 0;
+	user->vhb				= 0;
+	user->vkv				= 0;
+	user->vsp				= 0;
+	user->vspn				= 0;
+	user->vsp1				= 0;
+	user->vsp2				= 0;
+	user->vsp3				= 0;
+	user->w1stkl5			= 0;
+	user->w2stkl5			= 0;
+	user->w3stkl5			= 0;
+	user->x					= 0;
+	user->y					= 0;
+	user->zre4				= 0;
+	user->zre4j				= 0;
+	user->zre4vp			= 0;
+	user->ztabfb			= 0;
+	user->zvbez				= 0;
+	user->zvbezj			= 0;
+	user->zve				= 0;
+	user->zx				= 0;
+	user->zzx				= 0;
+
+	// eigene felder
+
+	user->rv				= 0;
+	user->av				= 0;
+	user->kv				= 0;
+	user->pv_an				= 0;
+	user->pv_ag				= 0;
+	user->kst				= 0;
+	user->gesamt_st			= 0;
+	user->sum_sv_an			= 0;
+	user->sum_sv_ag			= 0;
+	user->netto_lohn		= 0;
+	user->gesamt_belast_ag	= 0;
+
+	return user;
+};
---
+++ user_daten.hpp
@@ -0,0 +1,179 @@
+#ifndef _USER_DATEN_HPP_
+#define _USER_DATEN_HPP_ 1
+#include <assert.h>
+
+struct user_daten {
+
+//------------------------------------------------------
+// Eingabeparameter
+//------------------------------------------------------
+
+	int id_nummer;
+	int af;
+	int ajahr;
+	int alter1;
+	double entsch;
+	double f;
+	double jfreib;
+	double jhinzu;
+	double jre4;
+	double jre4ent;
+	double jvbez;
+	int krv;
+	double kvz;
+	int lzz;
+	double lzzfreib;
+	double lzzhinzu;
+	double mbv;
+	double pkpv;
+	int pkv;
+	int pvs;
+	int pvz;
+	int r;
+	double re4;
+	double sonstb;
+	double sonstent;
+	double sterbe;
+	int stkl;
+	double vbez;
+	double vbezm;
+	double vbezs;
+	double vbs;
+	int vjahr;
+	double vkapa;
+	double vmt;
+	double zkf;
+	int zmvb;
+
+//------------------------------------------------------
+// Ausgabeparameter
+//------------------------------------------------------
+
+	double bk;
+	double bks;
+	double bkv;
+	double lstlzz;
+	double solzlzz;
+	double solzs;
+	double solzv;
+	double sts;
+	double stv;
+	double vkvlzz;
+	double vkvsonst;
+
+//------------------------------------------------------
+// interne Felder or Ausgabeparameter DBA
+//------------------------------------------------------
+
+	double vfrb;
+	double vfrbs1;
+	double vfrbs2;
+	double wvfrb;
+	double wvfrbm;
+	double wvfrbo;
+
+//------------------------------------------------------
+// Interne Felder
+//------------------------------------------------------
+
+	double alte;
+	double anp;
+	double anteil1;
+	double bbgkvpv;
+	double bbgrv;
+	double bmg;
+	double diff;
+	double efa;
+	double fvb;
+	double fvbso;
+	double fvbz;
+	double fvbzso;
+	double gfb;
+	double hbalte;
+	double hfvb;
+	double hfvbz;
+	double hfvbzso;
+	double hoch;
+	int j;
+	double jbmg;
+	double jlfreib;
+	double jlhinzu;
+	double jw;
+	int k;
+	int kennvmt;
+	double kfb;
+	double kvsatzag;
+	double kvsatzan;
+	int kztab;
+	double lstjahr;
+	double lst1;
+	double lst2;
+	double lst3;
+	double lstoso;
+	double lstso;
+	double mist;
+	double pvsatzag;
+	double pvsatzan;
+	double rvsatzan;
+	double rw;
+	double sap;
+	double solzfrei;
+	double solzj;
+	double solzmin;
+	double solzsbmg;
+	double solzszve;
+	double solzvbmg;
+	double st;
+	double st1;
+	double st2;
+	double stovmt;
+	double tab1;
+	double tab2;
+	double tab3;
+	double tab4;
+	double tab5;
+	double tbsvorv;
+	double vbezb;
+	double vbezbso;
+	double vergl;
+	double vhb;
+	double vkv;
+	double vsp;
+	double vspn;
+	double vsp1;
+	double vsp2;
+	double vsp3;
+	int w1stkl5;
+	int w2stkl5;
+	int w3stkl5;
+	double x;
+	double y;
+	double zre4;
+	double zre4j;
+	double zre4vp;
+	double ztabfb;
+	double zvbez;
+	double zvbezj;
+	double zve;
+	double zx;
+	double zzx;
+
+	// eigene Felden
+
+	double rv;
+	double av;
+	double kv;
+	double pv_an;
+	double pv_ag;
+	double kst;
+	double gesamt_st;
+	double sum_sv_an;
+	double sum_sv_ag;
+	double netto_lohn;
+	double gesamt_belast_ag;
+
+};
+
+extern struct user_daten* new_User_daten();
+
+#endif
