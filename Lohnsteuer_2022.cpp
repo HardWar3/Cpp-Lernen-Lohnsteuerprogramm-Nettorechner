080966abf2264abbd910de2e35ede96280e44a7d
HardWar3 <mikewegner@gmx.net>
2022-06-07 20:48:54 +0200

    Lohnsteuer 2022

---
+++ Lohnsteuer_2022.cpp
@@ -0,0 +1,33 @@
+#include "user_daten.hpp"
+
+extern void mpara( struct user_daten* user );
+extern void mre4jl( struct user_daten* user );
+extern void mre4( struct user_daten* user );
+extern void mre4abz( struct user_daten* user );
+extern void mberech( struct user_daten* user );
+extern void msonst( struct user_daten* user );
+extern void mvmt( struct user_daten* user );
+
+
+int Lohnsteuer_2022( struct user_daten* user ) {
+
+	mpara( user );
+
+	mre4jl( user );
+
+	user->vbezbso = 0;
+
+	user->kennvmt = 0;
+
+	mre4( user );
+
+	mre4abz( user );
+
+	mberech( user );
+
+	msonst( user );
+
+	mvmt( user );
+
+	return 0;
+}
